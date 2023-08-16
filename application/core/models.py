6577e44be98f409f03dceb203d6028f0676d9445
ajpotts01 <ajpotts01@gmail.com>
2023-08-16 21:09:26 +1000

    Datetime => Django timezone, unit tests

--- application/core/models.py
+++ application/core/models.py
@@ -1,14 +1,12 @@
-# Standard lib imports
-import datetime as dt
-
 # Django imports
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Visit(models.Model):
     page: models.CharField = models.CharField(max_length=25)
-    visit_time: models.DateTimeField = models.DateTimeField(default=dt.datetime.utcnow)
+    visit_time: models.DateTimeField = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"Visited {self.page} on {self.visit_time}"
