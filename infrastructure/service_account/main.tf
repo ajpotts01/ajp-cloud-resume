80323eab10a8d97650f3f36c0171ede185b0284a
ajpotts01 <ajpotts01@gmail.com>
2023-08-18 18:56:01 +1000

    Fix cloud build log

--- infrastructure/service_account/main.tf
+++ infrastructure/service_account/main.tf
@@ -16,3 +16,9 @@ resource "google_project_iam_member" "cloud_build_storage_binding" {
   role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.cloud_resume_service_account.email}"
 }
+
+resource "google_project_iam_member" "cloud_build_service_agent_binding" {
+  project = var.project_id
+  role    = "roles/cloudbuild.serviceAgent"
+  member  = "serviceAccount:${google_service_account.cloud_resume_service_account.email}"
+}
