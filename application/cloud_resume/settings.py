8dd950acb26d2978924866132392031cd8ede8db
Alexander Potts <ajpotts01@gmail.com>
2023-09-03 13:40:05 +1000

    Most of the layout done

--- application/cloud_resume/settings.py
+++ application/cloud_resume/settings.py
@@ -146,8 +146,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'core/static/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Uploaded media
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
