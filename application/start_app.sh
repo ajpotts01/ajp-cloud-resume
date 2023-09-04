1cae705d9be5e0c4fad7fe7edd5f08ebdca6ae6d
Alexander Potts <ajpotts01@gmail.com>
2023-09-04 20:42:18 +1000

    Debugging bad litestream.yml

--- application/start_app.sh
+++ application/start_app.sh
@@ -1,10 +1,6 @@
 # https://usher.dev/posts/django-on-flyio-with-litestream-litefs/
-if [[ -z "$GCS_DB_URL" ]]; then
-    echo "S3_DB_URL env var not specified - this should be an S3-style URL to the location of the replicated database file"
-    exit 1
-fi
-
 echo $GCS_DB_URL
+ls
 litestream restore -if-db-not-exists -if-replica-exists -o "/tmp/db.sqlite3" "$GCS_DB_URL"
 
 python manage.py migrate
@@ -13,4 +9,4 @@ python manage.py collectstatic --verbosity 2 --no-input
 
 chmod -R a+rwX /tmp
 
-exec litestream replicate -config litestream.yml # Should execute the gunicorn command
+exec litestream replicate -config ./litestream.yml # Should execute the gunicorn command
