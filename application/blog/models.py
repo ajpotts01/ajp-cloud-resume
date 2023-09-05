637c88e6cc441b7d9cc24fc7af869ff96fcf5962
Alexander Potts <ajpotts01@gmail.com>
2023-09-05 21:11:31 +1000

    Basic image/gallery support

--- application/blog/models.py
+++ application/blog/models.py
@@ -1,13 +1,14 @@
+from typing import Any
+
 from django.db import models
 
-from wagtail.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
-
+from wagtail.models import Page, Orderable
 from wagtail.search import index
 
 
-# Create your models here.
 class HomePage(Page):
     body: RichTextField = RichTextField(blank=True)
 
@@ -16,12 +17,26 @@ class HomePage(Page):
 class BlogIndexPage(Page):
     intro: RichTextField = RichTextField(blank=True)
 
+    def get_context(self, request):
+        context: dict[str, Any] = super().get_context(request=request)
+        # Reverse chronological order
+        blog_pages = self.get_children().live().order_by("-first_published_at")
+        context["blog_pages"] = blog_pages
+
+        return context
+
     content_panels: list[FieldPanel] = Page.content_panels + [FieldPanel("intro")]
 
 class BlogPage(Page):
     date: models.DateField = models.DateField("Post date")
     intro: models.CharField = models.CharField(max_length=250)
     body: RichTextField = RichTextField(blank=True)
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        return None
 
     search_fields: list[index.SearchField] = Page.search_fields + [
         index.SearchField("intro"),
@@ -32,4 +47,17 @@ class BlogPage(Page):
         FieldPanel("date"),
         FieldPanel("intro"),
         FieldPanel("body"),
+        InlinePanel("gallery_images", label="Gallery images"),
+    ]
+
+class BlogPageGalleryImage(Orderable):
+    page: ParentalKey = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name="gallery_images")
+    image: models.ForeignKey = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    caption: models.CharField = models.CharField(blank=True, max_length=250)
+
+    panels: list[FieldPanel] = [
+        FieldPanel("image"),
+        FieldPanel("caption"),
     ]
