3b27ac88cf52de08eaae58d48c9ab34df01da761
Alexander Potts <ajpotts01@gmail.com>
2023-09-05 20:44:16 +1000

    Pulling in base assets from ordinary Wagtail project

--- application/blog/models.py
+++ application/blog/models.py
@@ -4,14 +4,32 @@ from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 
+from wagtail.search import index
+
 
 # Create your models here.
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    body: RichTextField = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [FieldPanel("body")]
+    content_panels: list[FieldPanel] = Page.content_panels + [FieldPanel("body")]
 
 class BlogIndexPage(Page):
-    intro = RichTextField(blank=True)
+    intro: RichTextField = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [FieldPanel("intro")]
+    content_panels: list[FieldPanel] = Page.content_panels + [FieldPanel("intro")]
+
+class BlogPage(Page):
+    date: models.DateField = models.DateField("Post date")
+    intro: models.CharField = models.CharField(max_length=250)
+    body: RichTextField = RichTextField(blank=True)
+
+    search_fields: list[index.SearchField] = Page.search_fields + [
+        index.SearchField("intro"),
+        index.SearchField("body"),
+    ]
+
+    content_panels: list[FieldPanel] = Page.content_panels + [
+        FieldPanel("date"),
+        FieldPanel("intro"),
+        FieldPanel("body"),
+    ]
