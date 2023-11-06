3e28c6d35559e5aff80e859bd4fcd45688f8cec3
Alexander Potts <ajpotts01@gmail.com>
2023-11-06 21:23:36 +1100

    Bye bye Django

--- infrastructure/secret_manager/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "app_name" {
-  description = "Name of the app - prefix for every resource"
-  type        = string
-}
-
-variable "region" {
-  description = "Region to create buckets in"
-  type        = string
-}
